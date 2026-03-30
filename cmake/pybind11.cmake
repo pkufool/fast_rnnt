a334cd38363287c48384d6c041f3656c9d9cd91e
pkufool <wkang.pku@gmail.com>
2026-03-30 19:04:07 +0800

    fix error

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -20,23 +20,12 @@ function(download_pybind11)
 
   include(FetchContent)
 
-  set(pybind11_URL  "https://github.com/pybind/pybind11/archive/v2.6.0.tar.gz")
-  set(pybind11_HASH "SHA256=90b705137b69ee3b5fc655eaca66d0dc9862ea1759226f7ccd3098425ae69571")
-
-  set(double_quotes "\"")
-  set(dollar "\$")
-  set(semicolon "\;")
-  if(NOT WIN32)
-    FetchContent_Declare(pybind11
-      URL               ${pybind11_URL}
-      URL_HASH          ${pybind11_HASH}
-    )
-  else()
-    FetchContent_Declare(pybind11
-      URL               ${pybind11_URL}
-      URL_HASH          ${pybind11_HASH}
-    )
-  endif()
+  # Keep pybind11 modern enough for recent CMake versions on CI.
+  FetchContent_Declare(pybind11
+    GIT_REPOSITORY https://github.com/pybind/pybind11.git
+    GIT_TAG        v2.13.6
+    GIT_SHALLOW    TRUE
+  )
 
   FetchContent_GetProperties(pybind11)
   if(NOT pybind11_POPULATED)
